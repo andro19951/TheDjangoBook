4bfc044d769ac7fd913cac4225b221514d65aaf9
andro199511 <andro19951@gmail.com>
2020-08-09 22:30:04 +0200

    form submission works now, you can add tasks on task app by submitting form

--- tasks/views.py
+++ tasks/views.py
@@ -5,7 +5,6 @@ tasks = ["foo","bar","bas"]
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 def index(request):
     return render(request, "tasks/index.html",{
@@ -13,6 +12,11 @@ def index(request):
         
         })
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task= form.cleaned_data["task"]
+            tasks.append(task)
     return render(request, "tasks/add.html", {
         'form': NewTaskForm()
         })
