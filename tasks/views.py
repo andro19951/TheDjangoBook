508b944bbf2f2d222f49ccbf3ab0f148a4779a44
andro199511 <andro19951@gmail.com>
2020-08-09 22:43:36 +0200

    utilizing empty condition in html adding information to session

--- tasks/views.py
+++ tasks/views.py
@@ -3,22 +3,26 @@ from django.http import HttpResponseRedirect
 from django import forms
 from django.urls import reverse
 # Create your views here.
-tasks = ["foo","bar","bas"]
+
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html",{
-        "tasks" : tasks
+        "tasks" : request.session["tasks"]
         
         })
 def add(request):
+
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task= form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
     return render(request, "tasks/add.html", {
         'form': NewTaskForm()
